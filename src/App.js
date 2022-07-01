5440f1fab130ee0a2a2fed3c6f9ab74814466bc1
DESKTOP-U3KS18B\hyosub <moneycf3@gmail.com>
2022-07-02 05:43:18 +0900

    내가가진 USD로 코인 얼마살수있는지 챌린지 완료

--- src/App.js
+++ src/App.js
@@ -1,57 +1,81 @@
 import { useEffect, useState } from "react";
+//import SelecBox from "./SelecBox";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
-  const [cost, setCost] = useState(1);
-  const [need, setNeed] = useState(1);
-  const onChange = (event) => {
-    setCost(event.target.value);
-    setNeed(1);
-  };
-  const handleInput = (event) => {
-    setNeed(event.target.value);
-  };
+  const [myMoney, setMyMoney] = useState(0);
+  const [calVal, setCalVal] = useState(0);
+  const [cPrice, setCprice] = useState(0);
+  const [cName, setCname] = useState("");
+
+  function onSubmit(event) {
+    event.preventDefault();
+    setCalVal(myMoney / cPrice);
+    let selIdx = event.target.selectBox.selectedIndex;
+    setCname(event.target.selectBox.options[selIdx].dataset.name);
+  }
+
+  function onChange(event) {
+    setMyMoney(event.target.value);
+  }
+  function changeCoin(event) {
+    console.log(event.target.value);
+    setCprice(event.target.value);
+  }
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
+      .then((response) => {
+        return response.json();
+      })
       .then((json) => {
         setCoins(json);
         setLoading(false);
+        setCprice(json[0].quotes.USD.price);
+        setCname(json[0].name);
       });
   }, []);
+
   return (
     <div>
-      <h1>The coins!{loading ? "" : `Here are..${coins.length} coins`}</h1>
-      {loading ? (
-        <strong>loading...</strong>
-      ) : (
-        <select onChange={onChange}>
-          <option>Select Coin!</option>
-          {coins.map((coin, index) => (
-            <option
-              key={index}
-              value={coin.quotes.USD.price}
-              id={coin.symbol}
-              symbol={coin.symbol}
-            >
-              {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <h2>Please enter the amount</h2>
-      <div>
+      <h1>The Coins {loading ? "" : `(${coins.length})`}</h1>
+      <form onSubmit={onSubmit}>
+        {loading ? (
+          <strong>Loading...</strong>
+        ) : (
+          <select onChange={changeCoin} name="selectBox" value={cPrice}>
+            {coins.map((coin, index) => (
+              <option
+                key={index}
+                value={coin.quotes.USD.price}
+                data-name={coin.symbol}
+              >
+                {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+              </option>
+            ))}
+          </select>
+        )}
+        <h1>How many coins you can buy</h1>
         <input
-          type="number"
-          value={need}
-          onChange={handleInput}
-          placeholder="dollor"
+          id="myMoney"
+          onChange={onChange}
+          value={myMoney}
+          placeholder="put your money.."
         />
-        $
-      </div>
-      <h2>You can get {need / cost}</h2>
+        <label htmlFor="myMoney">USD</label>
+        <button>Calculating!!!!!</button>
+        {calVal === 0 ? (
+          <h3>you can get nothing</h3>
+        ) : (
+          <h3>
+            you can get {calVal}&nbsp;&nbsp;&nbsp;
+            {cName}
+          </h3>
+        )}
+      </form>
     </div>
   );
 }
+
+// 내가가진 USD -> 몇개살수있는지 만들기
 export default App;
